372bbd5c663b120c85cf14ca42800b268beac399
Morad <morad-aoulad@hotmail.com>
2019-05-28 18:10:44 +0200

    Linq filtering en ordening toegevoegd

--- APIProject/Controllers/RatingController.cs
+++ APIProject/Controllers/RatingController.cs
@@ -24,6 +24,38 @@ namespace APIProject.Controllers
             return context.Ratings.ToList();
         }
 
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult<Rating> GetRating(int id)
+        {
+            if (id > 0)
+            {
+                return context.Ratings.Find(id);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("orderedbyrating")]
+        [HttpGet]
+        public List<Rating> GetRatingsOrdened()
+        {
+            var list = context.Ratings.ToList();
+            var ratingsSortedByValue = list.OrderByDescending(r => r.Value).ToList();
+            return ratingsSortedByValue;
+        }
+
+        [Route("bestratings")]
+        [HttpGet]
+        public List<Rating> GetBestRatings()
+        {
+            var list = context.Ratings.ToList();
+            var SeriesWith5StarRatings = list.Where(rating => rating.Value >= 5).ToList();
+            return SeriesWith5StarRatings;
+        }
+
         [HttpPost]
         public IActionResult CreateRating([FromBody] Rating newRating)
         {
